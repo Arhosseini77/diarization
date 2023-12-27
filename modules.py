98261aaef7f3d62875ff1b4241b8f739931169b6
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2023-12-27 10:48:47 +0330

    add reduce other speaker

--- modules.py
+++ modules.py
@@ -39,6 +39,25 @@ def extract_speaker_audio(diarization, speaker_label):
     return speaker_audio
 
 
+# Function to reduce volume of one speaker during overlap
+def reduce_overlap_volume(speaker_1_audio, speaker_2_audio):
+    overlap = speaker_1_audio.overlay(speaker_2_audio)
+
+    # Reduce the volume of the second speaker during the overlap
+    reduced_speaker_2 = overlap - speaker_2_audio
+
+    # Remove the original volume of the second speaker during the overlap
+    cleaned_audio = speaker_1_audio - overlap
+
+    # Combine the reduced volume speaker and cleaned audio
+    final_audio = cleaned_audio + reduced_speaker_2
+
+    return final_audio
+
+# Load the original audio file
+audio = AudioSegment.from_file(audio_file)
+
+
 # Extract audio for speaker 00
 speaker_00_audio = extract_speaker_audio(diarization, "SPEAKER_00")
 speaker_00_audio.export("./results/speaker_00_audio.mp3", format="mp3")
@@ -46,3 +65,9 @@ speaker_00_audio.export("./results/speaker_00_audio.mp3", format="mp3")
 # Extract audio for speaker 01
 speaker_01_audio = extract_speaker_audio(diarization, "SPEAKER_01")
 speaker_01_audio.export("./results/speaker_01_audio.mp3", format="mp3")
+
+# Reduce volume during overlap
+final_audio = reduce_overlap_volume(speaker_00_audio, speaker_01_audio)
+
+# Export the final audio
+final_audio.export("results/final_audio.mp3", format="mp3")
